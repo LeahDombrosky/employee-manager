150e26d3d9d6dce355749f6f6050acf99a28febe
Leah Dombrovsky <leahliya59@gmail.com>
2017-10-19 16:17:46 -0500

    working version

--- src/Stage 9/models/Employee.js
+++ src/Stage 9/models/Employee.js
@@ -1,9 +1,23 @@
 export default class Employee {
-  // constructor
+  constructor(id, name, phone, title) {
+    this.id = id;
+    this.name = name;
+    this.phone = phone;
+    this.title = title;
+  }
   
   // updateName
+  updateName(name) {
+    this.name = name;
+  }
 
   // updatePhone
+  updatePhone(phone) {
+    this.phone = phone;
+  }
 
    // updateTitle
+   pdateTitle(title) {
+    this.title = title;
+  }
 }
